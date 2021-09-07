37ab32a3c2c962f713244ca4887aa16eecee159d
charlesagyemang <micnkru@gmail.com>
2021-09-07 06:50:33 +0000

    list all incomes shit

---
+++ credentials/details.rb
@@ -0,0 +1,87 @@
+
+#######################################################
+
+  CUSTOMERS = [
+    {
+        "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO0N1c3RvbWVyOjUwMDMxMDky",
+        "name": "Abu Mohammed",
+    },
+    {
+        "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO0N1c3RvbWVyOjQ1OTI1Mzc2",
+        "name": "Frank Botwe (Blue And Yellow Car)",
+    },
+    {
+        "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO0N1c3RvbWVyOjQ1OTI1NjI4",
+        "name": "Promise Borklo (Yellow And Red)",
+    },
+    {
+        "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO0N1c3RvbWVyOjQ1OTI1Mjk1",
+        "name": "Isaac Essah (White Car)",
+    }
+  ]
+
+  PRODUCTS = [
+    {
+      "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO1Byb2R1Y3Q6NTc1OTU5NTU=",
+      "name": "Abrahama Weekly Sales",
+      "description": "Abraham (coffee and white color) weekly sales. 6 days of sales @GHC 1000 per day",
+    },
+    {
+      "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO1Byb2R1Y3Q6NTIzNzgwNjA=",
+      "name": "Blue Car Sales",
+      "description": "Blue Car Sales",
+    },
+    {
+      "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO1Byb2R1Y3Q6NTIzNzgxNjk=",
+      "name": "Red Car Sales ",
+      "description": "Red Car Sales",
+    },
+    {
+      "id": "QnVzaW5lc3M6MDkyMWM2NjItODlhMy00NDk3LTkzYTktNTI2MzE3MGUyNTcxO1Byb2R1Y3Q6NTIzNzc3MDc=",
+      "name": "White Car Weekly Sales",
+      "description": "White Car Weekly Sales",
+    }
+  ]
+
+  INCOME_ACCOUNTS = [
+    {
+      "id": "QWNjb3VudDoxMTQ0NjY4OTE5ODQwMTY0ODUzO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "Abraham(coffee and white truck) income",
+    },
+    {
+      "id": "QWNjb3VudDoxMDM4Nzg2MDg1NjUzNjI5NzE5O0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "Blue Car Sales",
+    },
+    {
+      "id": "QWNjb3VudDoxMDQ0NDA5NTcyOTg4NjAyNzUxO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "Red Car Sales",
+    },
+    {
+      "id": "QWNjb3VudDoxMDM4Nzg1NDkzODg3NjY2ODgwO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "White Car Sales",
+    }
+  ]
+
+  EXPENSE_ACCOUNTS = [
+    {
+      "id": "QWNjb3VudDoxMTM0NDUyNTkxMTkxNzA2NTU4O0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "wine and white truck expenses",
+    },
+    {
+      "id": "QWNjb3VudDoxMDM4NTc3MjAwNTk4NjY1ODExO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "Blue Car Expenses",
+    },
+    {
+      "id": "QWNjb3VudDoxMDM4NTc2ODI5OTE0NDY2ODM4O0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "Red Car Expenses",
+    },
+    {
+      "id": "QWNjb3VudDoxMDM4NTc2NDg0MjM2NzA4MzY0O0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+      "name": "White Car Expenses",
+    }
+  ]
+
+  BANK_AND_CASK = {
+    :vodafone_cash => "QWNjb3VudDoxMDQwMzcyMTI5NjE2NDE1MzAzO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ==",
+    :cash_on_hand => "QWNjb3VudDoxMDEzMDY0ODIyNDMxOTkyODMxO0J1c2luZXNzOjA5MjFjNjYyLTg5YTMtNDQ5Ny05M2E5LTUyNjMxNzBlMjU3MQ=="
+  }
